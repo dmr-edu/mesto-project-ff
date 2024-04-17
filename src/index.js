a396b1c8e8d156c5a594c7c66f9d2609b9f0f052
Rikov Dmitry <rikovdmitri39@gmail.com>
2024-04-17 20:15:46 +0300

    forms validation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import './pages/index.css';
 import { initialCards } from './scripts/cards';
 import { openPopup, closePopup, closePopupByBackdrop, closeClosestPopup } from './scripts/modal';
 import { createCard, likeCard, removeCard } from './scripts/card';
+import { clearValidation, enableValidation, toggleButtonState } from './scripts/validation';
+
+const formSelector = '.popup__form',
+  inputSelector = '.popup__input',
+  submitButtonSelector = '.popup__button',
+  inactiveButtonClass = 'popup__button_disabled',
+  inputErrorClass = 'popup__input_type_error',
+  errorClass = 'popup__error_visible'
 
 // Показать картинку в попапе
 const showImage = (data) => {
@@ -52,7 +60,28 @@ const popups = document.querySelectorAll('.popup');
 const openEditProfile = () => {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileDesc.textContent;
+  const inputList = Array.from(profileForm.querySelectorAll(inputSelector));
+  const buttonElement = profileForm.querySelector(submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, inactiveButtonClass)
+  clearValidation(profileForm, {
+    inputSelector,
+    inputErrorClass,
+    errorClass
+  })
   openPopup(editPopup)
+}
+
+const openNewCard = () => {
+  const inputList = Array.from(newCardPopup.querySelectorAll(inputSelector));
+  // Очистить сообщения об ошибках валидации если пользователь начинал вводить данные, а потом очистил поле и закрыл форму
+  if (inputList.every((input) => !input.value)) {
+    clearValidation(newCardPopup, {
+      inputSelector,
+      inputErrorClass,
+      errorClass
+    })
+  }
+  openPopup(newCardPopup)
 }
 
 /**
@@ -75,11 +104,16 @@ const addNewCardSubmit = (e) => {
   }, removeCard, likeCard, showImage);
   placesList.prepend(card);
   cardForm.reset();
+  clearValidation(cardForm, {
+    inputSelector,
+    inputErrorClass,
+    errorClass
+  })
   closePopup(newCardPopup);
 }
 
 editPopupBtn.addEventListener('click', openEditProfile);
-addCardBtn.addEventListener('click', () => openPopup(newCardPopup));
+addCardBtn.addEventListener('click', openNewCard);
 
 // Закрыть попап по кнопке "х"
 closePopupBtns.forEach((btn) => btn.addEventListener('click', closeClosestPopup));
@@ -87,8 +121,23 @@ closePopupBtns.forEach((btn) => btn.addEventListener('click', closeClosestPopup)
 // Закрыть попап по клику по фону
 popups.forEach((btn) => btn.addEventListener('click', closePopupByBackdrop));
 
+// Добавить модальным окнам класс "popup_is-animated"
+// чтобы избежать первоначальный переход прозрачности при загрузке страницы
+document.addEventListener('DOMContentLoaded', () => {
+  popups.forEach((p) => p.classList.add('popup_is-animated'))
+})
+
 // Сохранить изменения профиля
 profileForm.addEventListener('submit', handleProfileSubmit);
 
 // Добавить новую карточку
-cardForm.addEventListener('submit', addNewCardSubmit); 
+cardForm.addEventListener('submit', addNewCardSubmit);
+
+enableValidation({
+  formSelector,
+  inputSelector,
+  submitButtonSelector,
+  inactiveButtonClass,
+  inputErrorClass,
+  errorClass
+})
---
+++ src/scripts/validation.js
@@ -0,0 +1,88 @@
+const setEventListeners = (formElement, {
+  inputSelector,
+  submitButtonSelector,
+  inactiveButtonClass,
+  inputErrorClass,
+  errorClass
+}) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  const buttonElement = formElement.querySelector(submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, inactiveButtonClass)
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity({ formElement, inputElement, inputErrorClass, errorClass });
+      toggleButtonState(inputList, buttonElement, inactiveButtonClass)
+    });
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement, inactiveClass) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(inactiveClass)
+  } else {
+    buttonElement.classList.remove(inactiveClass)
+  }
+}
+
+function enableValidation(params) {
+  const formList = Array.from(document.querySelectorAll(params.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    const selectors = { ...params };
+    delete selectors.formSelector;
+    setEventListeners(formElement, selectors);
+  });
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+const showInputError = ({ formElement, inputElement, errorMessage, inputErrorClass, errorClass }) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(errorClass);
+};
+
+const hideInputError = ({ formElement, inputElement, inputErrorClass, errorClass }) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = ({
+  formElement,
+  inputElement,
+  inputErrorClass,
+  errorClass
+}) => {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMsg);
+  } else {
+    inputElement.setCustomValidity("");
+  }
+  if (!inputElement.validity.valid) {
+    showInputError({ formElement, inputElement, errorMessage: inputElement.validationMessage, inputErrorClass, errorClass });
+  } else {
+    hideInputError({ formElement, inputElement, inputErrorClass, errorClass });
+  }
+};
+
+function clearValidation(formElement, { inputSelector, inputErrorClass, errorClass }) {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  inputList.forEach((inputElement) => {
+    hideInputError({ formElement, inputElement, inputErrorClass, errorClass })
+  });
+}
+
+export {
+  toggleButtonState,
+  clearValidation,
+  enableValidation
+}
